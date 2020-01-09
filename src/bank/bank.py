bde8123d5bb975b36e8564a96d0e012c7f183226
Zdenda <tkmushroomer@gmail.com>
2020-01-09 16:40:11 +0100

    NaN: ZeroMQ pridana do banky. Cast logiky implementovana.

--- src/bank/bank.py
+++ src/bank/bank.py
@@ -1,45 +1,15 @@
 #
-# This script will simulate the bank server. It will accept transaction orders
-# from the shuffler, sort them by their id and execute them. There will be a couple
-# of instances of this server running at the same time and each of them will use its'
-# own DB.
-#
-# API offered by bank is described in doc/bank-api.yml.
-#
-# I used Bottle (https://falcon.readthedocs.io/en/stable/) framework for the API.
-# It can be installed by 
-# pip install bottle
-#
-# Test it using curl:
-#  curl -X POST --header "Content-Type: application/json" --data '{"amount":5, "id":1}' localhost:8100/credit 
+# This script will simulate the bank server. It will send/receive CREDIT and DEBIT messages
+# with random amounts (interval <10000;50000>) to/from its' direct peers. Every DB server has 
+# it's own DB.
+# 
+# Banks use ZeroMQ to communicate with eachother.
 #
 
-from bottle import Bottle, template, request, HTTPResponse
 import mysql.connector
-import heapq
 import logging
-
-class Transaction:
-	"""
-	Wrapper for bank transaction so that heap can be used.
-	"""
-	
-	def __init__(self, id, amount, type):
-		self.id = id
-		self.amount = amount
-		self.type = type
-		
-	def __cmp__(self, other):
-		return self.id < other.id
-		
-	def __lt__(self, other):
-		return self.id < other.id
-		
-	def is_debit(self):
-		return self.type == "debit"
-		
-	def to_string(self):
-		return "{id: %s, amount: %s, type: %s}" % (self.id, self.amount, self.type)
+import zmq
+from random import randrange
 
 class DbConnector:
 	"""
@@ -105,84 +75,161 @@ class DbConnector:
 		cursor.execute("select balance from account")
 		return cursor.fetchone()
 		
-		
+
+class Message:
+	"""
+	Message sent between banks
+	"""
+	
+	def __init__(self, type, amount):
+		self.type = type
+		self.amount = amount
 
 class Bank:
 	"""
 	Implementation of the bank server.
 	"""
 	
-	def __init__(self, host, port, debug, db_connector):
+	def __init__(self, host, port, debug, db_connector, other_banks):
 		"""
 		Initializes this server with given values.
+		
+		:param string host: IP address of this bank.
+		:param port: Port this bank should listen on. If None, bank will not expect any connections.
+		:param list other_banks: List of banks this one hould connect to via ZeroMQ. Each entry should be in format <host>:<port>.
 		"""
 		self._host = host
 		self._port = port
 		self._debug = debug
 		self._db_connector = db_connector
-		self._expected_id = 0
-		self._transaction_heap = []
-		self._app = Bottle()
-		self._route()
-	
-	def start(self):
-		"""
-		Starts the server.
-		"""
-		self._app.run(host=self._host, port=self._port, debug = self._debug)
-	
-	def _route(self):
-		"""
-		Sets up routes to endpoints.
-		"""
-		self._app.route('/credit', method="POST", callback=self._credit)
-		self._app.route('/debit', method="POST", callback=self._debit)		
+		self._context = zmq.Context()
 		
-	def _credit(self):
-		"""
-		Handler for /credit API.
-		"""
-		transaction = request.json
-		logging.info("Credit: " + str(transaction))
-		self._add_transaction(Transaction(transaction["id"], transaction["amount"], "credit"))
-		return HTTPResponse(status = 200)
-	
-	def _debit(self):
-		"""
-		Handler for /debit API.
-		"""
-		transaction = request.json
-		logging.info("Debit: " + str(transaction))
-		self._add_transaction(Transaction(transaction["id"], transaction["amount"], "debit"))
-		return HTTPResponse(status = 200)
-	
-	def _add_transaction(self, transaction):
-		"""
-		Adds transaction to the heap and if the expected id is at the top of heap
-		the transaction is executed.
+		# socket to given peer can be accessed as _peers["host:port"]
+		self._peers = []
 		
-		:param Transaction transaction: Transaction to be added to the queue.
+		self._init_queues(other_banks)
+	
+	def _init_queues(self, other_banks):
 		"""
-		heapq.heappush(self._transaction_heap, transaction)
+		Initializes connections to other banks and starts to listen on given port (if the port is set).
+		"""
 		
-		# expected transaction id -> pop from heap and execute it
-		curr_balance = None
-		while len(self._transaction_heap) > 0 and self._transaction_heap[0].id == self._expected_id:
-			t = heapq.heappop(self._transaction_heap)
-			logging.info("Executing transaction: %s" % t.to_string())
-			if t.is_debit():
-				self._db_connector.debit_money(t.amount)
-			else:
-				self._db_connector.credit_money(t.amount)
-				
-			self._expected_id = self._expected_id + 1
-			curr_balance = self._db_connector.get_amount()
+		# start listening if port is set
+		if self._port is not None:
+			self._socket = self._context.socket(zmq.PAIR)
+			self._socket.bind("tcp://*:%s" % port)
+			self._poller.register(self._socket)
+		else:
+			self._socket = None
 			
-		if curr_balance is not None:
-			with open("balance.txt", "w") as out_f:
-				out_f.write(str(curr_balance[0]))
-				out_f.write("\n")
+		# connect to neighbours
+		self._poller = zmq.Poller()
+		for other_bank in other_banks:
+			s = self._context.socket(zmq.PAIR)
+			s = self._context.connect("tcp://%s" % other_bank)
+			self._peers.append(s)
+			self._poller.register(s)
 			
+	def _start_server(self):
+		"""
+		Starts banking server - message sending and receiving.
+		"""
+		
+		logging.info("Starting receive/send loop.")
+		self._should_run = True
+		while self._should_run:
+			self._recv_messages()
+			self._generate_message()
+			
+		logging.info("Loop finished gracefully.")
+			
+	
+	def _generate_message(self):
+		"""
+		Generate and send one message to direct neighbor.
+		"""
+		logging.info("Generating message.")
+		amount = 10000 + randrange(40001)
+		
+		# choose target to send message to
+		# either main socket this bank is listening on
+		# or one of the peers this bank is connected to
+		peer_cnt = len(self._peers)
+		total_sockets = peer_cnt + (1 if self._socket is not None else 0)
+		target = None
+		rand = randrange(total_sockets)
+		if rand < peer_cnt:
+			target = self._peers[rand]
+		else:
+			target = self._socket
+		
+		if randrange(2) == 0:
+			self._send_credit(amount, target)
+		else:
+			self._send_debit(amount, target)
+	
+	def _recv_messages(self):
+		"""
+		Poll for messages from ZeroMQ. Timeout is between 10 and 100 ms.
+		"""
+		
+		t = 10 + randrange(101)
+		socks = dict(self._poller.poll(timeout = t))
+		if len(socks) > 0:
+			logging.info("%d sockets polled." % len(socks))
+			# todo: go through polled sockets and process messages
+			# todo: convert json to Message 
+		
+		
+	def _process_messages(self, message):
+		"""
+		Process one message from queue.
+		
+		:param message: Message received from queue.
+		"""
+		if message.type == "CREDIT":
+			self._credit(message.amount)
+		else:
+			self._debit(message.amount)
+	
+	def _credit(self, amount):
+		"""
+		Credits given amount to this bank.
+		"""
+		logging.info("Credit: " + str(amount))
+		self._db_connector.credit_money(amount)
+	
+	def _debit(self, amount, target):
+		"""
+		Sends given amount of money back to target or sends REFUSE if there's not enough money in the account.
+		"""
+		logging.info("Debit: " + str(amount) + " message from " + str(target))
+		self._send_credit(amount, target)
+	
+	def _send_credit(self, amount, target):
+		"""
+		Deducts given amount from this bank's account and sends CREDIT message to target.
+		
+		:param Socket target: Socket to send message to.
+		"""
+		curr_amount = self._db_connector.get_amount()
+		if curr_amount < amount:
+			self._send_refuse(target)
+		else:
+			self._db_connector.debit_money(amount)
+			target.send_json(Message("CREDIT", amount))
+			
+	def _send_debit(self, amount, target):
+		"""
+		Sends DEBIT message for given amount to given target.
+		"""
+		target.send_json(Message("DEBIT", amount))
+		
+	def _send_refuse(self, target):
+		"""
+		Sends REFUSED message to target.
+		"""
+		target.send_json(Message("REFUSED", -1))
 		
 				
 		
